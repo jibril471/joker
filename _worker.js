28ba69b9b18a4c5c48d8c67c5c4bb936bb47a735
Bonchell <64392667+jibril471@users.noreply.github.com>
2026-03-09 01:17:25 +0700

    Update root domain in _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "bonthot227.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "riyanjibril227.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "joker"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
